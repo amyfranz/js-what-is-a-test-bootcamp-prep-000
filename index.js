ed17687ae48cc93b79ef9de8457e8cb366e5c1af
Amy Franz <amy.franz@gmail.com>
2019-11-28 11:56:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+
+function happyHolidays () {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo (name) {
+  return"Happy holidays, " + ${name} + "!"
+}
+
+function happyHolidayTo (name, holiday) {
+  return 'Happy ${holiday}, ${name}!'
+}
+
+function holidayCountdown (holiday, days) {
+  return "It's ${days} days until ${holiday}!"
+}
